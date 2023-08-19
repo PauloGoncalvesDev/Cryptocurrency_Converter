122a5d9617ec348e522e6b40d31ef1623bb4a84c
Paulo Guilherme <paulogoncalves.dev@gmail.com>
2023-08-19 10:25:34 -0300

    BUG_1 - Ajusta responsividade texto

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,26 @@
 import Menu from "../components/MenuItem/MenuFormItem";
 import Form from "../components/Form/Form";
 import { AppContext } from "../components/Contexts/AppContext/AppContext";
+import { Theme, ThemeProvider, Typography, createTheme } from "@mui/material";
 
 function Home() {
+  const theme: Theme = createTheme();
+  theme.typography.h2 = {
+    fontSize: "2rem",
+    [theme.breakpoints.up("md")]: {
+      fontSize: "3rem",
+    },
+  };
+
   return (
     <>
       <Menu />
       <AppContext>
-        <h1>Converter Criptomoedas</h1>
+        <ThemeProvider theme={theme}>
+          <Typography variant="h2" lineHeight="1.1" fontWeight="bold">
+            Converter Criptomoedas
+          </Typography>
+        </ThemeProvider>
         <Form />
       </AppContext>
     </>
